7675ae425324f0b95ad33ac75027dcd2e8b4dd11
Seyoung <happy_712sy@naver.com>
2024-03-12 00:11:53 +0900

    6: delete 항목에 대한 click event

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ function render() {
                             <div class="task-done">${taskList[i].taskContent}</div>
                                 <div>
                                     <button onClick="toggleComplete('${taskList[i].id}')">Check</button>
-                                    <button>Delete</button>
+                                    <button onClick="deleteTask('${taskList[i].id}')">Delete</button>
                                 </div>
                             </div>`
         } else {
@@ -51,7 +51,7 @@ function render() {
                             <div>${taskList[i].taskContent}</div>
                                 <div>
                                     <button onClick="toggleComplete('${taskList[i].id}')">Check</button>
-                                    <button>Delete</button>
+                                    <button onClick="deleteTask('${taskList[i].id}')">Delete</button>
                                 </div>
                             </div>`
         }
@@ -63,13 +63,26 @@ function render() {
 function toggleComplete(id){
     for (let i=0; i<taskList.length; i++){
         if(taskList[i].id == id){ //list의 id와, 매개변수의 id(check여부) 비교
-            taskList[i].isComplete = true;
+            taskList[i].isComplete = !taskList[i].isComplete; // 클릭한 순간 taskList[i].isComplete의 값과 반대값으로 변경 (true -> false / false -> true)
             break;
         }
     }
-    render();
+    render(); //★
     console.log(taskList);
 }
+
+//item의 delete 버튼을 클릭 시, 해당 item을 제거하기
+function deleteTask(id){
+    for(let i=0; i<taskList.length; i++){
+        if(taskList[i].id == id){
+            taskList.splice(i,1); //i번째 아이템 1개만 삭제
+            break;
+        }
+    }
+    render(); //★
+    console.log(taskList);
+}
+
 
 //중복되지 않는 랜덤한 아이디 생성
 function randomIdGenerate(){
