e64b15a6314a59ec116a5faa2d4becb39dd72281
Seyoung <happy_712sy@naver.com>
2024-03-14 23:54:57 +0900

    7: tab click event

--- main.js
+++ main.js
@@ -1,23 +1,32 @@
-//유저가 값을 입력한다.
-// [+] 버튼을 '클릭'하면, 할일이 추가(객체)
-// [delete] 버튼을 '클릭'하면, 할일이 삭제
-// [check] 버튼을 '클릭'하면 할일이 끝나며, 밑줄이 그어짐 :: render()에서 click event
-    //(1) 버튼을 클릭하는 순간 isComplete이 false -> true로 바꿈
-    //(2) true이면 끝난걸로 간주하고 밑줄
-    //(3) false이면 안끝난걸로 간주하고 그대로.
-// [ING] 탭에서 -> [DONE] 탭을 누르면, 언더바가 이동
-// [DONE] 탭은 끝난 아이템만, [ING]은 진행중 아이템만
-// [ALL] 탭을 누르면 다시 전체 아이템으로 돌아옴
-
+/*
+1. 유저가 값을 입력한다.
+2. [+] 버튼을 '클릭'하면, 할일이 추가(객체)
+3. [delete] 버튼을 '클릭'하면, 할일이 삭제
+4. [check] 버튼을 '클릭'하면 할일이 끝나며, 밑줄이 그어짐 :: render()에서 click event
+    (1) 버튼을 클릭하는 순간 isComplete이 false -> true로 바꿈
+    (2) true이면 끝난걸로 간주하고 밑줄
+    (3) false이면 안끝난걸로 간주하고 그대로.
+5. [ING] 탭에서 -> [DONE] 탭을 누르면, 언더바가 이동
+6. [DONE] 탭은 끝난 아이템만, [ING]은 진행중 아이템만
+7. [ALL] 탭을 누르면 다시 전체 아이템으로 돌아옴
+*/
 
 
 let taskInput = document.getElementById("task-input");
 let addButton = document.getElementById("add-button");
+let tabs = document.querySelectorAll(".task-tabs div"); //task-tabs의 div 여러개를 선택
 let taskList = []; //할 일
 
 addButton.addEventListener("click", addTask);
 
+//tab 변경
+for(let i=1;i<tabs.length;i++){ //console.log 해본 결과 0은 under-line이기 때문에, 1부터 시작(ALL,ING,DONE)
+    tabs[i].addEventListener("click",function(event){
+        filter(event); // event: 어떤 탭을 클릭했는지에 대한 정보
+    });
+}
 
+console.log(tabs);
 //할일을 추가하는 함수
 function addTask(){
     //let taskContent = taskInput.value;
@@ -81,6 +90,10 @@ function deleteTask(id){
     }
     render(); //★
     console.log(taskList);
+}
+
+function filter(event){ //event : 어떤 탭을 클릭했는지에 대한 정보
+    console.log("filter");
 }
 
 
